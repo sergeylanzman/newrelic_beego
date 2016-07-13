c584503b860691152692b3f4a77fe771f2345f21
Sergey Lanzman <Sergeylanz@gettaxi.com>
2016-07-13 21:51:44 +0300

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+newrelic_beego
+=============
+
+newrelic_beego is "plug and play" package for monitoring beego framework with newrelic offical agent(beta)
+
+# Installation
+```
+go get github.com/sergeylanzman/newrelic_beego"
+```
+
+Add  _ "github.com/sergeylanzman/newrelic_beego" to import in main.go file
+
+# Settings
+    - appname = name of app in newrelic
+    - newrelic_license = newrelic license
+    - newrelic_beta_token = newrelic beta token(currently must)
---
+++ newrelic_beego.go
@@ -0,0 +1,52 @@
+package newrelic_beego
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+	"github.com/newrelic/go-agent"
+)
+
+var NewrelicAgent newrelic.Application
+
+func StartTransaction(ctx *context.Context) {
+	re := regexp.MustCompile("[0-9]{2,}")
+	path := re.ReplaceAllString(ctx.Request.URL.Path, ":id")
+	txName := fmt.Sprintf("%s %s", ctx.Request.Method, path)
+	tx := NewrelicAgent.StartTransaction(txName, ctx.ResponseWriter, ctx.Request)
+	ctx.Input.SetData("newrelic_transaction", tx)
+}
+func EndTransaction(ctx *context.Context) {
+	if ctx.Input.GetData("newrelic_transaction") != nil {
+		tx := ctx.Input.GetData("newrelic_transaction").(newrelic.Transaction)
+		tx.End()
+	}
+}
+
+func init() {
+	appName := beego.AppConfig.String("appname")
+	license := beego.AppConfig.String("newrelic_license")
+	betaToken := beego.AppConfig.String("newrelic_beta_token")
+	if license == "" {
+		beego.Warn("Please set NewRelic license in config(newrelic_license)")
+		return
+	}
+	if betaToken == "" {
+		beego.Warn("Please set NewRelic beta token in config(newrelic_beta_token)")
+		return
+	}
+	config := newrelic.NewConfig(appName, license)
+	config.BetaToken = betaToken
+	app, err := newrelic.NewApplication(config)
+	if err != nil {
+		beego.Warn(err.Error())
+		return
+	}
+
+	NewrelicAgent = app
+	beego.InsertFilter("*", beego.BeforeRouter, StartTransaction, false)
+	beego.InsertFilter("*", beego.FinishRouter, EndTransaction, false)
+	beego.Info("NewRelic agent start")
+}
